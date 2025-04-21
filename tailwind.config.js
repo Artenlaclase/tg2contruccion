fd961377be93788981cff6050453497cc7ee2064
Artenlaclase <artenlaclase@gmail.com>
2025-04-20 21:25:21 -0400

    Add Tailwind CSS configuration and global styles

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    content: ["./src/**/*.{astro,html,js,jsx,ts,tsx}"],
+    theme: {
+      extend: {
+        fontFamily: {
+          sans: ['"Inter"', 'sans-serif'],
+        },
+        colors: {
+          primary: "#111111",
+          accent: "#ffffff",
+        },
+      },
+    },
+    plugins: [],
+  }
+  
